0a49d547da8d6cc14df0d02a1cbdc797d920acd7
fferrlop <fferrlop@myuax.com>
2023-11-29 18:42:31 +0100

    bibliotecas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <algorithm>
+#include <valarray>
+#include <memory>
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
