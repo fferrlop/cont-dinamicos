f5d014d9c5854b1a9777d15a7934bc3c52f66d44
fferrlop <fferrlop@myuax.com>
2023-11-29 19:17:39 +0100

    Ej.1.3

--- main.cpp
+++ main.cpp
@@ -10,14 +10,22 @@
 int main() {
 
 //Ejercicio 1:
+std::cout << "Ejercicio 1: " << std::endl;
+
     std::vector<int> Vector;
 
-    Vector.push_back(5);
-    Vector.push_back(10);
-    Vector.push_back(15);
-    Vector.push_back(20);
-    Vector.push_back(25);
-    Vector.push_back(30);
+        Vector.push_back(5);
+        Vector.push_back(10);
+        Vector.push_back(15);
+        Vector.push_back(20);
+        Vector.push_back(25);
+        Vector.push_back(30);
+
+    std::cout << "Elementos del vector: " << std::endl;
+    for (std::vector<int>::iterator it = Vector.begin(); it != Vector.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
