731d31e47946365c06896815d8793b752d4cdd8d
fferrlop <fferrlop@myuax.com>
2023-11-29 19:30:46 +0100

    Ej.3.1

--- main.cpp
+++ main.cpp
@@ -48,6 +48,10 @@ std::cout << "Ejercicio 3: " << std::endl;
     std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
     std::sort(numeros.begin(), numeros.end());
 
+    for(int num : numeros) {
+        std::cout << num << std::endl;
+    }
+
 
     return 0;
 }
