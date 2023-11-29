454c1e7799cd57a4047420da385174640d771f13
fferrlop <fferrlop@myuax.com>
2023-11-29 19:16:11 +0100

    Ej.1.2

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ int main() {
     std::vector<int> Vector;
 
     Vector.push_back(5);
+    Vector.push_back(10);
+    Vector.push_back(15);
+    Vector.push_back(20);
+    Vector.push_back(25);
+    Vector.push_back(30);
 
     return 0;
 }
